2160d91baab4b235f8793c5805879705baad5489
Tomas Hudak <hudakt05@hotmail.com>
2020-02-01 17:50:06 +0000

    tt

--- GGJ2020/Assets/Scripts/CryoScreenScript.cs
+++ GGJ2020/Assets/Scripts/CryoScreenScript.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class CryoScreenScript : MonoBehaviour
 {
-    [SerializeField] Color deadColour;
-    [SerializeField] Color aliveColour;
+    [SerializeField] Material deadMat;
+    [SerializeField] Material aliveMat;
     [SerializeField] GameObject ThisCryoSocket;
-
+    [SerializeField] Text Text;
     [SerializeField] string DeadText;
+
+    [SerializeField] string AliveText;
+
+    bool alive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,20 @@ public class CryoScreenScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!ThisCryoSocket.GetComponent<WireSocketLogic>().IsColourMatched())
+        {
+            alive = false;
+        }
+
+        if (alive)
+        {
+            Text.text = AliveText;
+            GetComponent<MeshRenderer>().material = aliveMat;
+        }
+        else
+        {
+            Text.text = DeadText;
+            GetComponent<MeshRenderer>().material = deadMat;
+        }
     }
 }
