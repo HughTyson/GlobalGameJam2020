8401f8733daf8c3781fedfad18d371804ebffa11
Dominik Frankowski <1701542@uad.ac.uk>
2020-02-01 15:14:49 +0000

    Game loop now exists. There is a bug after one playthrough in the end screen mouse disappears and buttons not clickable.

--- GGJ2020/Assets/Scripts/MainGameLoop.cs
+++ GGJ2020/Assets/Scripts/MainGameLoop.cs
@@ -1,20 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class MainGameLoop : MonoBehaviour
 {
+    public Text timerString;
+    private float timerFloat;
+    private bool playOnce;
 
-    
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
+    // Initialise the text object
+    void Start()
+    {
+        timerFloat = 6.0f;
+        timerString.text = timerFloat.ToString();
+        playOnce = true;
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    void Update()
+    {
+        // Only start the timer if the player is ready
+        if (GetComponent<GameLoop>().GetFinishedFadeOut())
+            UpdateTimer();
+
+        // Do something at the end of the timer
+        //  Reload the scene at the end of the timer
+        if (timerFloat <= 0.0f)
+        {
+            if (playOnce)
+            {
+                gameObject.GetComponent<GameLoop>().PlayFadeIn();
+                playOnce = false;
+            }   
+
+            if(GetComponent<GameLoop>().GetFinishedFadeIn())
+                gameObject.GetComponent<ChangeScene>().ReloadGameScene();
+        }
+
+        // Sample code of how the main game loop should go
+        /*
+         * if(Winning condition)
+         * {
+         *  gameObject.GetComponent<ChangeScene>().LoadEndScene();
+         * }
+         * else if (timerFloat <= 0f || Some losing condition)
+         * {
+         *  gameObject.GetComponent<ChangeSccene>().ReloadGameScene();
+         * }
+         * 
+         * 
+         */
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "win")
+            gameObject.GetComponent<ChangeScene>().LoadEndScene();
+    }
+
+    private void UpdateTimer()
+    {
+        // While the timer is greater than 0 decrease it and convert it to a string.
+        if(timerFloat > 0.0f)
+        {
+            timerFloat -= Time.deltaTime;
+            timerString.text = timerFloat.ToString("F5");
+        }
+    }
 }
