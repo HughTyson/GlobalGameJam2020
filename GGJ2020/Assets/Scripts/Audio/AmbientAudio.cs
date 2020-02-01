fb68462dcbeabe515f5ad19107b469d39607cb3c
1601564 <55140108+1601564@users.noreply.github.com>
2020-02-01 00:44:13 +0000

    AudioClips
    Added generator hum and clang (stand ins atm)

--- GGJ2020/Assets/Scripts/Audio/AmbientAudio.cs
+++ GGJ2020/Assets/Scripts/Audio/AmbientAudio.cs
@@ -4,34 +4,14 @@ using UnityEngine;
 
 public class AmbientAudio : MonoBehaviour
 {
-    //Fade in and out sounds/music
-    //static IEnumerator Fade(AudioSource source, float duration, float targetVolume)
-    //{
-    //    //Set the current time and get the sources current volume
-    //    float currentTime = 0;
-    //    float startVolume = source.volume;
-
-
-    //    //decrease/increase the volume within the time limit
-    //    while (currentTime < duration)
-    //    {
-    //        currentTime += Time.deltaTime;
-    //        //Smoothly go from the current volume to the expected one
-    //        source.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
-    //        yield return null;
-    //    }
-    //    yield break;
-    //}
-
+  
     IEnumerator playAudioSequentially()
     {
         yield return null;
 
-        //1.Loop through each AudioClip
-        for (int i = 0; i < adClips.Count; i++)
-        {
+       
             //2.Assign current AudioClip to audiosource
-            adSource[0].clip = adClips[i];
+            adSource[0].clip = adClips[Random.Range(0, adClips.Count)];
 
             //3.Play Audio
             adSource[0].Play();
@@ -42,8 +22,6 @@ public class AmbientAudio : MonoBehaviour
                 yield return null;
             }
 
-            //5. Go back to #2 and play the next audio in the adClips array
-        }
     }
 
     IEnumerator playEffectsSequentially()
