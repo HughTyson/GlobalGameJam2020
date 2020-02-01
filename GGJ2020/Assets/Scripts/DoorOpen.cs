9cb70cc3f8b98854efbf7858284b06a48670ca26
BrodieT <35993664+BrodieT@users.noreply.github.com>
2020-02-01 12:31:26 +0000

    Doors done

---
+++ GGJ2020/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    [SerializeField] private GameObject Door1;
+    [SerializeField] private GameObject Door2;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            OpenDoors();
+        }
+    }
+
+    public void OpenDoors()
+    {
+        StartCoroutine(Open());
+    }
+
+    IEnumerator Open()
+    {
+        if (Door1 != null)
+        {
+            Door1.GetComponent<Animator>().enabled = true;
+            Door1.GetComponent<Animator>().Play("DoorAnim");
+        }
+        else
+        {Debug.LogError("Invalid Door 1");
+        }
+
+        yield return new WaitForSeconds(2);
+
+        if (Door2 != null)
+        {
+            Door2.GetComponent<Animator>().enabled = true;
+            Door2.GetComponent<Animator>().Play("DoorAnim");
+        }
+
+    }
+}
